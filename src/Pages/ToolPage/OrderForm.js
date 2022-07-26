b211614cc80f22bd692f37b35a00973cf5a517ce
Anwar Sagor <sagor.onnet@gmail.com>
2022-07-26 14:38:01 +0600

    Orderinfo post by server

--- src/Pages/ToolPage/OrderForm.js
+++ src/Pages/ToolPage/OrderForm.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const OrderForm = ({ tool, setTool }) => {
     const { _id, name, img, price, quantity, description } = tool;
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const [user, loading, error] = useAuthState(auth);
-
+    const navigate = useNavigate()
 
     const onSubmit = data => {
         console.log(data)
-        // const url = `https://infinite-badlands-56898.herokuapp.com/product`;
-        // fetch(url, {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(data)
-        // })
-        //     .then(res => res.json())
-        //     .then(result => {
-        //         console.log(result);
-        //     })
+
+        const url = `http://localhost:5000/order`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+        navigate('/tools');
     };
 
 
@@ -33,10 +36,10 @@ const OrderForm = ({ tool, setTool }) => {
                 <h2 className='text-center font-bold text-xl'>Submit Your Information</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="grid grid-clos-1 gap-1">
-                        <input className="input input-bordered w-full max-w-xs" value={user.displayName} {...register("name")} />
+                        <input className="input input-bordered w-full max-w-xs" value={user?.displayName || ''} {...register("name")} />
                         <input className="input input-bordered w-full max-w-xs" placeholder='Address' {...register("address")} />
                         <input className="input input-bordered w-full max-w-xs" placeholder='Mobile' {...register("mobile")} />
-                        <input className="input input-bordered w-full max-w-xs" value={user.email} {...register("email")} />
+                        <input className="input input-bordered w-full max-w-xs" value={user?.email || ''} {...register("email")} />
                         <input className="input input-bordered w-full max-w-xs" placeholder='Price' type="number" {...register("price")} />
                         <input className="input input-bordered w-full max-w-xs" placeholder='Quantity' type="number" {...register("quantity")} />
                         <input className="input input-bordered w-full max-w-xs" placeholder='Photo URL' type="text" {...register("img")} />
