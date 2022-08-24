2a8e6d7b0b12b763f974372caa2a49839b8cac1d
Anwar Sagor <sagor.onnet@gmail.com>
2022-08-25 00:30:41 +0600

    repo from private to public

--- src/App.js
+++ src/App.js
@@ -27,47 +27,51 @@ function App() {
 
   return (
     <div >
-      <Header></Header>
+      <div className='sticky top-0 z-50 mb-16'>
+        <Header></Header>
+      </div>
 
-      <Routes>
+      <div>
+        <Routes>
 
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/tools' element={<Tools></Tools>}></Route>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/tools' element={<Tools></Tools>}></Route>
 
-        <Route path='/tool/:toolId' element={
-          <RequireAuth>
-            <Purchase></Purchase>
-          </RequireAuth>
-        }></Route>
+          <Route path='/tool/:toolId' element={
+            <RequireAuth>
+              <Purchase></Purchase>
+            </RequireAuth>
+          }></Route>
 
-        <Route path='/dashboard' element={
-          <RequireAuth>
-            <Dashboard></Dashboard>
-          </RequireAuth>
-        }>
-          <Route index element={<MyOrders></MyOrders>}></Route>
-          <Route path='myOrders' element={<MyOrders></MyOrders>}></Route>
-          <Route path='addReview' element={<AddReview></AddReview>}></Route>
-          <Route path='myProfile' element={<MyProfile></MyProfile>}></Route>
-          <Route path='users' element={<Users></Users>}></Route>
-          <Route path='addTool' element={<AddTool></AddTool>}></Route>
-          <Route path='deleteTool' element={<DeleteTool></DeleteTool>}></Route>
-          <Route path='payment/:id' element={<Payment></Payment>}></Route>
-          <Route path='cancel/:id' element={<Cancel></Cancel>}></Route>
-        </Route>
+          <Route path='/dashboard' element={
+            <RequireAuth>
+              <Dashboard></Dashboard>
+            </RequireAuth>
+          }>
+            <Route index element={<MyOrders></MyOrders>}></Route>
+            <Route path='myOrders' element={<MyOrders></MyOrders>}></Route>
+            <Route path='addReview' element={<AddReview></AddReview>}></Route>
+            <Route path='myProfile' element={<MyProfile></MyProfile>}></Route>
+            <Route path='users' element={<Users></Users>}></Route>
+            <Route path='addTool' element={<AddTool></AddTool>}></Route>
+            <Route path='deleteTool' element={<DeleteTool></DeleteTool>}></Route>
+            <Route path='payment/:id' element={<Payment></Payment>}></Route>
+            <Route path='cancel/:id' element={<Cancel></Cancel>}></Route>
+          </Route>
 
-        <Route path='about' element={<About></About>}></Route>
-        <Route path='blogs' element={<Blogs></Blogs>}></Route>
+          <Route path='about' element={<About></About>}></Route>
+          <Route path='blogs' element={<Blogs></Blogs>}></Route>
 
-        <Route path='/signIn' element={<SignIn></SignIn>}></Route>
-        <Route path='/signUp' element={<SignUp></SignUp>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
+          <Route path='/signIn' element={<SignIn></SignIn>}></Route>
+          <Route path='/signUp' element={<SignUp></SignUp>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
 
-        <Route path='/test' element={<Test></Test>}></Route>
-        <Route path='' element></Route>
-      </Routes>
-      <ToastContainer />
+          <Route path='/test' element={<Test></Test>}></Route>
+          <Route path='' element></Route>
+        </Routes>
+        <ToastContainer />
+      </div>
 
     </div>
   );
--- src/Pages/HomePage/HomeTools.js
+++ src/Pages/HomePage/HomeTools.js
@@ -8,14 +8,14 @@ const HomeTools = (tool) => {
 
     return (
         <div className='lg:px-12 my-4' >
-            <h2 className='font-extrabold text-center text-5xl text-transparent bg-clip-text bg-gradient-to-b from-blue-900 to-cyan-300'>YOUR TOOLS</h2>
+            <h2 className='font-extrabold text-center text-5xl text-transparent bg-clip-text bg-gradient-to-b from-blue-900 to-cyan-300'>TOOLS</h2>
 
             <div className='grid lg:grid-cols-5 md:grid-rows-1 gap-4'>
                 {
                     tools.slice(0, 5).map(tool =>
                         <div key={tool._id} className="card card-compact my-5 bg-base-100 shadow-xl">
                             <figure className='h-60'>
-                                <img className='max-h-60 p-2' src={tool.img} alt="Tool Image" />
+                                <img className='max-h-60 p-2' src={tool.img} alt="Tool" />
                             </figure>
                             <div className="card-body">
                                 <h2 className="card-title text-transparent bg-clip-text bg-gradient-to-b from-blue-900 to-cyan-300"><p className='text-center'>{tool.name}</p></h2>
